b9c3ddc44877c5839e755bc3ba11e0143501b710
tanerijun <tanerivince@gmail.com>
2022-12-16 14:58:49 +0800

    Refactor ls cmd

--- cmd/ls.go
+++ cmd/ls.go
@@ -13,24 +13,28 @@ var listCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "Show list of TODOs",
 	Run: func(cmd *cobra.Command, args []string) {
-		todos, err := db.AllTodos()
-		if err != nil {
-			fmt.Println("Error reading from database:", err)
-			os.Exit(1)
-		}
-
-		if len(todos) == 0 {
-			fmt.Println("You are free from todos :)")
-			return
-		}
-
-		fmt.Println("Here are you list of todos:")
-		for i, todo := range todos {
-			fmt.Printf("%d. %s\n", i+1, todo.Value)
-		}
+		handleLs()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+}
+
+func handleLs() {
+	todos, err := db.AllTodos()
+	if err != nil {
+		fmt.Println("Error reading from database:", err)
+		os.Exit(1)
+	}
+
+	if len(todos) == 0 {
+		fmt.Println("You are free from todos :)")
+		return
+	}
+
+	fmt.Println("Here are you list of todos:")
+	for i, todo := range todos {
+		fmt.Printf("%d. %s\n", i+1, todo.Value)
+	}
 }
